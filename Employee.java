3fd8dd0b47d4bf042aa299ceb0263843d2bac891
quizera81 <kwizeraeric919@gmail.com>
2019-12-29 14:16:18 +0200

    added new client class and new employee class

---
+++ Employee.java
@@ -0,0 +1,14 @@
+public interface  Employee{
+    // every user should have a name
+    public void setName(String name);
+    public String getName();
+
+    // every user should have 
+    public void setId(int id);
+    public int getId();
+    // Every worker should have salary
+    public void setSalary(float Salary);
+    public float getSalary();
+     
+
+}
